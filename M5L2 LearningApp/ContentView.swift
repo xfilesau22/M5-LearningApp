f828ff5eb398f45134862962bc71a3c17b5f9849
AlanDinon <awdinon@bigpond.com>
2022-04-26 11:05:32 +1000

    Initial Commit

---
+++ M5L2 LearningApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  M5L2 LearningApp
+//
+//  Created by Alan Dinon on 26/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ M5L2 LearningApp/M5L2_LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  M5L2_LearningAppApp.swift
+//  M5L2 LearningApp
+//
+//  Created by Alan Dinon on 26/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct M5L2_LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
